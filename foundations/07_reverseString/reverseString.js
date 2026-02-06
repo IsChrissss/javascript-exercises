1cd9619884ee57444fca43dd7cca1c935fd1917f
Ionut Cristian Enoiu <cristianionut2015@gmail.com>
2026-02-06 20:14:38 +0100

    Exercise 7

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let array = string.split(" ").reverse()
+    let reverseArrayString = array.map(word => {
+        return word.split("").reverse().join("");
+    })
+    return reverseArrayString.join(" ");
 };
 
 // Do not edit below this line
