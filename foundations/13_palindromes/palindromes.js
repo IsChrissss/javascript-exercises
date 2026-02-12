5f907b72b836846735ea24f3be198acdf868525d
Ionut Cristian Enoiu <cristianionut2015@gmail.com>
2026-02-12 21:26:44 +0100

    Exercise 13 completed

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+   const cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+   const reverseStr = cleanStr.split('').reverse().join('')
+   return reverseStr === cleanStr;
 };
 
 // Do not edit below this line
