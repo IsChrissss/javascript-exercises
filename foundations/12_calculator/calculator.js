ea04f75cbc8169bc539dfeb2f5212b967dc7e494
Ionut Cristian Enoiu <cristianionut2015@gmail.com>
2026-02-12 21:17:27 +0100

    Exercise 11 completed

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if(arr.length === 0) {
+    return 0
+  }
+	let sum = 0;
+  for (const num of arr) sum += num;
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if(arr.length === 0) {
+    return 1;
+  }
+  let mult = 1;
+  for (const num of arr) mult *= num;
+  return mult;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 1 || num === 0) {
+    return 1;
+  }
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
