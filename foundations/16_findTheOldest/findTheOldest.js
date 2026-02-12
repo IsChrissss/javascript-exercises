034f140d2c9930d9ddbe84e5ad19a46c982af1da
Ionut Cristian Enoiu <cristianionut2015@gmail.com>
2026-02-12 22:01:13 +0100

    Exrecise 16 completed

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldAge, newAge) => {
+        const oldestAge = getAge(oldAge.yearOfBirth, oldAge.yearOfDeath);
+        const newestAge = getAge(newAge.yearOfBirth, newAge.yearOfDeath);
+        return oldestAge > newestAge ? oldAge : newAge;
+    })
+};
 
+// Helper function to handle living people (using current year)
+const getAge = (birth, death) => {
+  if (!death) death = new Date().getFullYear();
+  return death - birth;
 };
 
 // Do not edit below this line
